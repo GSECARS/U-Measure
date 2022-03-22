68f0f9070a1fd1c953aa907e7dceb9867c9eda60
skordaschristofanis <skordas.chr@hotmail.com>
2022-03-22 13:56:49 -0500

    Updated custom lines.

--- measure/widget/custom/q_line.py
+++ measure/widget/custom/q_line.py
@@ -3,14 +3,16 @@ from typing import Optional
 
 
 class QLine(QFrame):
+    """Custom vertical/horizontal line widget to expand to the available space."""
+
     def __init__(self, vertical: Optional[bool] = True) -> None:
         super(QLine, self).__init__()
 
         self._vertical = vertical
-
         self._set_orientation()
 
     def _set_orientation(self) -> None:
+        """Sets the orientation of the line to vertical or horizontal."""
         if self._vertical:
             self.setFrameShape(QFrame.Shape.VLine)
         else:
