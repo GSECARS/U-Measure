1979238821d2e5cd107af4b9064b24efcfbaf93e
skordaschristofanis <skordas.chr@hotmail.com>
2022-03-22 13:36:51 -0500

    Added control status group.

---
+++ measure/widget/groups/__init__.py
@@ -0,0 +1,4 @@
+from measure.widget.groups.setup_widget import SetupWidget
+from measure.widget.groups.experiment_widget import ExperimentWidget
+from measure.widget.groups.control_status_widget import ControlStatusWidget
+from measure.widget.groups.main_group_widget import MainGroupWidget
